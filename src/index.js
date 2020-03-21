9ab2cb0b02076121f9443ac44acde025a811c2c5
Priuet <nancy56@list.ru>
2020-03-21 08:15:39 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsConfigLength = bracketsConfig.length;
+    for (let i = 0; i < bracketsConfigLength;) {
+        let brackets = bracketsConfig[i].join('');
+        if (str.includes(brackets)) { 
+            str = str.replace(brackets, ''); 
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    let result = str.length ? false : true;
+    return result;
 }
